056691e28c13ce9c8dd82db3f7f628dc8efcf2cb
Emma Stotz <stotz.emma@gmail.com>
2019-11-24 21:27:53 -0500

    Touched up nav bar styling.

--- meme-ory/src/App.js
+++ meme-ory/src/App.js
@@ -1,14 +1,9 @@
 import React, { Component } from "react";
 // Components
-import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
 import MemeCard from "./components/MemeCard";
 import memes from "./memes.json";
 import Nav from "./components/Nav";
-// Bootstrap Styling Elements
-import Column from "./components/Column";
-import Row from "./components/Row";
-import Container from "./components/Row";
 
 function shuffleMemes(array) {
   for (let i = array.length - 1; i > 0; i--) {
@@ -64,32 +59,34 @@ class App extends Component{
 
   render() {
     return (
-      <Wrapper>
-        <Nav
-          image = {this.state.image}
-          score = {this.state.currentScore}
-          topScore = {this.state.topScore}
-        />
-
+      <div className="wrapper">
+        <div className="container container-fluid">
+          <Nav
+            image = {this.state.image}
+            score = {this.state.currentScore}
+            topScore = {this.state.topScore}
+          />
+        </div>  
         <Title>
           Try to click on each meme only once.
         </Title>
 
-        <Container>
-          <Row>
-            {this.state.memes.map(meme => (
-              <Column size="sm-3 md-4">  
-                <MemeCard
-                  key = {meme.id}
-                  id = {meme.id}
-                  image = {meme.image}
-                  handleClick = {this.handleClick}
-                />
-              </Column>
-            ))}
-          </Row>
-        </Container>
-      </Wrapper>
+        <div className="container meme-container">
+          <div className="row">
+          {this.state.memes.map(meme => (
+            <div className="col-sm-4">
+
+              <MemeCard
+                key = {meme.id}
+                id = {meme.id}
+                image = {meme.image}
+                handleClick = {this.handleClick}
+              />
+            </div>
+          ))}
+          </div>
+        </div>
+      </div>
     )
   };
 };
--- meme-ory/src/components/Nav/index.js
+++ meme-ory/src/components/Nav/index.js
@@ -1,28 +1,36 @@
 import React from "react";
 import "./style.css";
-import logo from "./logo.svg"
+import logo from "./logo-01.png";
 
-const Nav = props => (
-  <nav className="navbar">
-    <ul className="nav justify-content-end">
+const Nav = props => {
+  return (
+  <nav className="navbar navbar-expand-lg">
+    <ul className="navbar justify-content-end">
       <li className="nav-item">
         <a 
           href="_blank" 
           className="navbar-brand">
-          <img 
+          <img width="250" height="auto" 
             src={logo}
             alt="logo"
-          ></img>
+          />
         </a>
       </li>
+      <li></li>
+      <li></li>
+      <li></li>
       <li className="nav-item" id="cur-score">
-        Current Score: {props.score}
+        <span class="navbar-text">
+          Current Score: {props.score}
+        </span>
       </li>
       <li className="nav-item" id="top-score">
-        Top Score: {props.topScore}
+        <span class="navbar-text">
+          Top Score: {props.topScore}
+        </span>
       </li>
     </ul>
   </nav>
-);
+)};
 
 export default Nav;
