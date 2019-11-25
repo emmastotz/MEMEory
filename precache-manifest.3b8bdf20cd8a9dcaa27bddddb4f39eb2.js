6970e2b7ea20a9737c9730149f5b3b63fdf2eb80
Emma Stotz <stotz.emma@gmail.com>
2019-11-24 21:40:37 -0500

    Updates

---
+++ precache-manifest.3b8bdf20cd8a9dcaa27bddddb4f39eb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f72815591849fc55362ed6d1085d4966",
+    "url": "/MEMEory/index.html"
+  },
+  {
+    "revision": "2cd7b25a7681658c74a4",
+    "url": "/MEMEory/static/css/main.2a4d0aa4.chunk.css"
+  },
+  {
+    "revision": "d480f345b34281c264e7",
+    "url": "/MEMEory/static/js/2.3025f259.chunk.js"
+  },
+  {
+    "revision": "2cd7b25a7681658c74a4",
+    "url": "/MEMEory/static/js/main.0113c79d.chunk.js"
+  },
+  {
+    "revision": "3463449d4d60cf982d9e",
+    "url": "/MEMEory/static/js/runtime-main.585ca169.js"
+  },
+  {
+    "revision": "f883a604907a70b2d1e7ef6b4655c1f1",
+    "url": "/MEMEory/static/media/logo-01.f883a604.png"
+  }
+]);
